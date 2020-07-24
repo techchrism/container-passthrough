9d298d3ca5e65f38e1d621bb89aa8b32c1ef3af2
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-07-24 09:43:54 -0700

    add sign blocks to passthrough

--- src/main/java/com/darkender/plugins/containerpassthrough/ContainerPassthrough.java
+++ src/main/java/com/darkender/plugins/containerpassthrough/ContainerPassthrough.java
@@ -1,6 +1,9 @@
 package com.darkender.plugins.containerpassthrough;
 
 import org.bukkit.FluidCollisionMode;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Container;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -21,6 +24,8 @@ import java.util.Set;
 public class ContainerPassthrough extends JavaPlugin implements Listener
 {
     private Set<EntityType> passthroughEntities;
+    private Set<Material> passthroughBlocks;
+    private boolean ignoreInteractEvents = false;
     
     @Override
     public void onEnable()
@@ -28,7 +33,28 @@ public class ContainerPassthrough extends JavaPlugin implements Listener
         passthroughEntities = new HashSet<>();
         passthroughEntities.add(EntityType.PAINTING);
         passthroughEntities.add(EntityType.ITEM_FRAME);
+        
+        passthroughBlocks = new HashSet<>();
+        for(Material material : Material.values())
+        {
+            if(material.name().contains("SIGN"))
+            {
+                passthroughBlocks.add(material);
+            }
+        }
+        
         getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    private boolean canOpenContainer(Player player, Block block, BlockFace face)
+    {
+        // Send out an event to ensure container locking plugins aren't bypassed
+        ignoreInteractEvents = true;
+        PlayerInteractEvent interactEvent = new PlayerInteractEvent(player, Action.RIGHT_CLICK_BLOCK,
+                player.getInventory().getItemInMainHand(), block, face, EquipmentSlot.HAND);
+        getServer().getPluginManager().callEvent(interactEvent);
+        ignoreInteractEvents = false;
+        return !(interactEvent.useInteractedBlock() == Event.Result.DENY || interactEvent.isCancelled());
     }
     
     private boolean tryOpeningContainer(Player player)
@@ -40,11 +66,7 @@ public class ContainerPassthrough extends JavaPlugin implements Listener
         }
         Container container = (Container) result.getHitBlock().getState();
         
-        // Send out an event to ensure container locking plugins aren't bypassed
-        PlayerInteractEvent interactEvent = new PlayerInteractEvent(player, Action.RIGHT_CLICK_BLOCK,
-                player.getInventory().getItemInMainHand(), result.getHitBlock(), result.getHitBlockFace(), EquipmentSlot.HAND);
-        getServer().getPluginManager().callEvent(interactEvent);
-        if(interactEvent.useInteractedBlock() == Event.Result.DENY || interactEvent.isCancelled())
+        if(!canOpenContainer(player, result.getHitBlock(), result.getHitBlockFace()))
         {
             return false;
         }
@@ -68,5 +90,29 @@ public class ContainerPassthrough extends JavaPlugin implements Listener
         {
             event.setCancelled(true);
         }
+    }
+    
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    private void onPlayerInteract(PlayerInteractEvent event)
+    {
+        if(ignoreInteractEvents || event.getPlayer().isSneaking() ||
+                event.getAction() != Action.RIGHT_CLICK_BLOCK ||
+                event.getClickedBlock() == null ||
+                !passthroughBlocks.contains(event.getClickedBlock().getType()))
+        {
+            return;
+        }
+    
+        Block blockBehind = event.getClickedBlock().getRelative(event.getBlockFace().getOppositeFace());
+        if(!(blockBehind.getState() instanceof Container) || !canOpenContainer(event.getPlayer(), blockBehind, event.getBlockFace()))
+        {
+            return;
+        }
+        Container container = (Container) blockBehind.getState();
+        if(!event.getPlayer().getOpenInventory().getTopInventory().equals(container.getInventory()))
+        {
+            event.getPlayer().openInventory(container.getInventory());
+        }
+        event.setCancelled(true);
     }
 }
